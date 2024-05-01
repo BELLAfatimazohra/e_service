d8cf9dd7d8f819af8653f38abb300750474406f2
Abderrahmane <ab.amm209@gmail.com>
2024-05-01 15:29:54 +0100

    new homepage

--- etudiant/empTmps/empTmps.php
+++ etudiant/empTmps/empTmps.php
@@ -3,41 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="home/Home.css">
+    <link rel="stylesheet" href="empTmps.css">
     <title>e-Service</title>
 </head>
 <body>
 <header>
-    <div class="group">
-        <a class="profile" href="../profile/profile.php"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/></svg></a>
-    </div>
-        
-        
-        <!-- <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success searchButton" type="submit">Search</button>
-      </form> -->
-
-
-        
+        <!--<nav id="topbar">
+            <ul>
+                <li><a href="#notes">Mes Notes</a></li>
+                <li><a href="#emploi-du-temps">Emploi du Temps</a></li>
+                <li><a href="#ressources">Ressources Pédagogiques</a></li>
+                <li><a href="#support">Support</a></li>
+                <li><a href="#profil">Mon Profil</a></li>
+            </ul>
+        </nav>-->
     </header>
-
-
-
 
 
     <nav id="sidebar">
         <div class="imgcontainer">
-            <img class="logo" src="../public/images/logo-ensah.png" alt="ensah">
+            <img class="logo" src="../../public/images/logo-ensah.png" alt="ensah">
         </div>
       <ul class="nav-list">
-        <li class="active">
-          <a href="index.php" aria-current="page">
+        <li>
+          <a href="../index.php" aria-current="page">
           <div class="group"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/></svg>Acceuille</div>  
           <div class="arrow-left"></div></a>
         </li>
-        <li>
-          <a href="empTmps/empTmps.php"><div class="group">
+        <li  class="active">
+          <a href="empTps.php"><div class="group">
             <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M320-400q-17 0-28.5-11.5T280-440q0-17 11.5-28.5T320-480q17 0 28.5 11.5T360-440q0 17-11.5 28.5T320-400Zm160 0q-17 0-28.5-11.5T440-440q0-17 11.5-28.5T480-480q17 0 28.5 11.5T520-440q0 17-11.5 28.5T480-400Zm160 0q-17 0-28.5-11.5T600-440q0-17 11.5-28.5T640-480q17 0 28.5 11.5T680-440q0 17-11.5 28.5T640-400ZM200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Z"/></svg>Emploi du temps
           </div><div class="arrow-left"></div></a>
           </li>
